590af642aa6a975127f5c0ae1c0438881a6a7c8d
Lu Yin <luyin@Lus-MacBook-Air.local>
2015-05-04 12:15:26 -0700

    fixed syntax errors

--- client/app/article/article.js
+++ client/app/article/article.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('contextcommentsApp')
   .config(function ($stateProvider) {
     $stateProvider
@@ -11,15 +12,15 @@ angular.module('contextcommentsApp')
         		controller: function() {}
 	    },
 
-        	"content@article": { 
-        		template: "app/article/article.html",
+        	'content@article': { 
+        		template: 'app/article/article.html',
         		controller: function() {}
         },
-        	"comment@article": { 
-        		template: "app/article/article.html",
+        	'comment@article': { 
+        		template: 'app/article/article.html',
         		controller: function() {}
         	}
         },
         // templateUrl: 'app/article/article.html',
-      })
+      });
   });
