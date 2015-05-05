3406bb26e720859c3324f7edb079b89b6524f125
Derek Sakamoto <dmsakamoto@gmail.com>
2015-05-05 09:35:10 -0700

    Added the routing and set up for 2 views of the comment box and comment entries

--- client/app/article/comment/comment.controller.js
+++ client/app/article/comment/comment.controller.js
@@ -3,6 +3,24 @@
 console.log('here');
 
 angular.module('contextcommentsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('comment', {
+        url: '/comment',
+        views: {
+
+          '':{
+            templateUrl: 'app/article/comment/comment.html',
+            controller: function() {}
+      },
+          'commentEntry@comment': { 
+            templateUrl: 'app/article/comment/commentEntry/comment.html',
+            controller: 'CommentEntryCtrl'
+          }
+        },
+        // templateUrl: 'app/article/article.html',
+      })
+  })
   .controller('CommentBoxCtrl', function ($scope, $http) {
     $scope.entries = [{name : 'Dave', value: 'a'},{name : 'Dave2', value: 'a2'}
     ];
