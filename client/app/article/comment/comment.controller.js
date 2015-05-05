6625220b955e598349b26a24bc0b924e0976bcb1
Derek Sakamoto <dmsakamoto@gmail.com>
2015-05-05 11:45:34 -0700

    Updated html and cleaned up code for the database boxes. TODO: Clean up CSS/Bootstrap panels to put the comment boxes on the right of the page

--- client/app/article/comment/comment.controller.js
+++ client/app/article/comment/comment.controller.js
@@ -6,19 +6,17 @@ angular.module('contextcommentsApp')
   .controller('CommentBoxCtrl', function ($scope, $http) {
     $scope.boxes = [
       {
-        name: 'Comment Box 1', 
-        value: 'A', 
+        commentedText: 'Comment Box 1', 
         entries: [
-          {user: 'John', comment: 'Hello, World!'}, 
-          {user: 'Jane', comment: 'Hello, John!'}
+          {username: 'John', comment: 'Hello, World!', updatedAt: 'Today'}, 
+          {username: 'Jane', comment: 'Hello, John!', updatedAt: 'Today'}
         ]
       },
       {
-        name: 'Comment Box 2', 
-        value: 'B',
+        commentedText: 'Comment Box 2', 
         entries: [
-          {user: 'John', comment: 'Jane you are not the world.'}, 
-          {user: 'Jane', comment: 'How do you know?'}
+          {username: 'John', comment: 'Jane you are not the world.', updatedAt: 'Today'}, 
+          {username: 'Jane', comment: 'How do you know?', updatedAt: 'Today'}
         ]
       }
     ];
